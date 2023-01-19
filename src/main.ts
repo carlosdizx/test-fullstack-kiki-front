755996d2ac129f98e71501c9502edf5116fe16e5
Carlos Ernesto Diaz <carlodiaz@umariana.edu.co}>
2023-01-19 11:41:26 -0500

    installed vue-axios and configured

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,12 @@ import App from './App.vue'
 import { createApp } from 'vue'
 import { registerPlugins } from '@/plugins'
 import router from "@/routes";
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
 const app = createApp(App)
+
+app.use(VueAxios, axios)
 
 app.use(router)
 
