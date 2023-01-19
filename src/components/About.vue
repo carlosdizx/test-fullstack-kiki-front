086056fdc62ba091dc393cab392bf9dddbba4237
Carlos Ernesto Diaz <carlodiaz@umariana.edu.co}>
2023-01-19 10:42:02 -0500

    Created componet for show info about pokemon

---
+++ src/components/About.vue
@@ -0,0 +1,12 @@
+<template>
+<v-card>
+  <v-card-text><h2>Los Pokemon tambien son personas</h2></v-card-text>
+</v-card>
+</template>
+
+<script lang="ts" setup>
+</script>
+
+<style scoped>
+
+</style>
--- src/view/Home.vue
+++ src/view/Home.vue
@@ -1,6 +1,9 @@
 <template>
-<h1>Hola</h1>
+<v-container>
+  <About></About>
+</v-container>
 </template>
 
 <script lang="ts" setup>
+import About from "@/components/About.vue";
 </script>
